dd16fb2e789f933216430bc8d286125273eea472
Antoine Brossault <abrossault@abrossault-macbookair.roam.corp.google.com>
2018-04-09 17:21:13 +0100

    Now searching in HTML + CSS : more footprints added and more tests

--- js-reader.js
+++ js-reader.js
@@ -6,6 +6,8 @@ const { URL } = require('url');
 
 const chalk = require('chalk');
 
+const _ = require('lodash');
+
 
 
 const init = (domain) => {
@@ -19,10 +21,18 @@ const init = (domain) => {
             if( ! response.statusCode === 200 && ! body ) return reject('Wrong satus code or body response');
                 
             const jsFiles = getJsUrlfromDocument(body,domain);
-    
-            if( !jsFiles && !jsFiles.length > 0) return reject('No JavaScript files');
-    
-            const proms = requestsJS( jsFiles );
+
+            const cssFiles = getCSSfromDocument(body,domain);
+
+            let  proms = []
+
+            if( cssFiles ) proms.push( requestsURI( cssFiles) );
+
+            if( jsFiles )  proms.push( requestsURI( jsFiles) );
+
+            proms.push(searchInHTML(body,domain));
+
+            proms = _.flatten(proms);
 
             Promise
                 .all(proms)
@@ -47,16 +57,20 @@ const init = (domain) => {
 
 
 
-
-const requestsJS = (jsList) => {
+/**
+ * 
+ * @param {array} list contains the urls 
+ * @returns {promise}
+ */
+const requestsURI = (list) => {
 
     var promises = [ ];
     
-    jsList = excludeScripts(jsList);
+    list = excludeScripts(list);
 
-    jsList.forEach( s => console.log( chalk.grey( "👉 Script detected ", s ) ) );
+    list.forEach( s => console.log( chalk.grey( "👉 File detected ", s ) ) );
 
-    jsList.forEach( jsUrl => promises.push(  requestAndSearch(jsUrl) ) );
+    list.forEach( url => promises.push(  requestAndSearch(url) ) );
 
     return promises;
 
@@ -65,26 +79,21 @@ const requestsJS = (jsList) => {
 
 
 
-const requestAndSearch = (jsUrl) => {
+const requestAndSearch = (url) => {
 
     return new Promise( (resolve, reject) => { 
 
-        request( jsUrl , (error, response, body)  => {
+        request( url , (error, response, body)  => {
         
             if( typeof response == "undefined" ||  error ) return reject(error);
     
-            if( ! response.statusCode === 200 && ! body )  return reject('Wrong satus code or body response');
-                    
-            return resolve({
-                url : jsUrl,
-                adyen : (/adyen\.encrypt/).test(body),
-                payplug : (/payplug/).test(body),
-                stripe : (/Stripe\.version/).test(body),
-                braintree : (/Braintree\.version/).test(body),
-                paypal : (/PAYPAL/).test(body),
-                laterpay: (/.laterpay\.net/).test(body),
-                 //  window\.adyen
-            });
+            if( response.statusCode !== 200 && ! body )  return reject('Wrong status code or body response');
+            
+            let searchResult = searchForFootprint(body)
+
+            searchResult.url = url
+
+            return resolve(searchResult);
         
         });
 
@@ -128,6 +137,40 @@ const getJsUrlfromDocument  = (body,domain) => {
 
 
 
+const getCSSfromDocument  = (body,domain) => {
+
+    const $ = cheerio.load(body);
+
+    var css = [];
+        
+    try {
+        
+        $('link[rel=stylesheet]').each( function(i, elem){
+
+            const href = $(this).attr('href');
+        
+            if( typeof href !== "undefined"){
+
+                css.push( formatUrl(href,domain) );
+
+            }  
+            
+        });
+
+
+    } catch (error) {
+
+        console.log( chalk.red('Cheerio failed ', error ) )
+
+    }
+
+
+    return ( css.length > 0 ) ? css : false;
+
+}
+
+
+
 
 const formatUrl = (src,domain) => {
 
@@ -143,6 +186,9 @@ const excludeScripts = urls => {
 
     const excluded =  ['jquery','optimizely','abtasty','googleadservices.com','bootstrap','googleapis.com','addthis.com','jsdelivr.net','fontawesome.com','trustpilot.com'];
     
+
+    if( urls.includes("tricks")) console.log("________", urls);
+
     return urls.filter( u => {
   
         let foundExclusion = false;
@@ -165,8 +211,48 @@ const excludeScripts = urls => {
 
 
 
+const searchInHTML = (body,url) =>{
+
+    const prom = new Promise( (resolve, reject) =>{
+
+        let footPrint = searchForFootprint(body);
+        footPrint.url = url;
+        
+        resolve(footPrint);
+
+    });
+    
+    return [prom];
+
+}
+
+
+
+const searchForFootprint = (body,url) =>{
+
+
+    return {
+            adyen : (/adyen\.encrypt/).test(body),
+            payplug : (/payplug/i).test(body),
+            stripe : (/Stripe\.version/).test(body),
+            braintree : (/Braintree\.version/).test(body),
+            //paypal : (/PAYPAL/).test(body),
+            laterpay: (/.laterpay\.net/).test(body),
+            ogoneIngenico: (/(ogone|ingenico)/i).test(body),
+            be2bill: (/be2bill/i).test(body),
+            hipay: (/hipay/i).test(body),
+            payline: (/payline/i).test(body),
+            atos: (/atos/).test(body), // css #atosPaymentFrame
+        }
+}
+
+
+
 
 
 module.exports = {
-    init : init
+    init : init,
+    test : {
+        getCSSfromDocument : getCSSfromDocument
+    }
 }
--- tests/tests.js
+++ tests/tests.js
@@ -1,37 +1,173 @@
-
 const assert = require('assert');
 const jsReader = require('../js-reader');
 const _ = require('lodash');
+const fs = require('fs');
 
 const chalk = require('chalk');
+
+
 
 
 describe(' Analyze a website ' , function() {
 
    
-    it('should fin Adyen in redmart.com files ', function(done) {
+    it('should find Adyen in redmart.com files ', function(done) {
         
         this.timeout(105000);
 
         const url = "redmart.com"
-        
     
         jsReader.init(url)
             .then( r => {
                 
                 let script = _.find( r, ['adyen', true ] );
 
-                console.log( chalk.grey(script) );
-
                 assert.equal(script.adyen, true);
 
             })
             .then(done, done)
-        
-     
-        
 
     });
 
 
+
+
+    it('should find Ogone in collectorsquare.com HTML ', function(done) {
+        
+        this.timeout(105000);
+
+        const url = "collectorsquare.com";
+    
+        jsReader.init(url)
+            .then( r => {
+                
+                let script = _.find( r, ['ogoneIngenico', true ] );
+
+                assert.equal(script.ogoneIngenico, true);
+
+            })
+            .then(done, done)
+    });
+
+
+    it('should find Ogone in collectorsquare.com HTML ', function(done) {
+        
+        this.timeout(105000);
+
+        const url = "collectorsquare.com";
+    
+        jsReader.init(url)
+            .then( r => {
+                
+                let script = _.find( r, ['ogoneIngenico', true ] );
+
+                assert.equal(script.ogoneIngenico, true);
+
+            })
+            .then(done, done)
+    });
+
+
+
+
+    it('should find be2bill in maliterie.com  ', function(done) {
+    
+        this.timeout(105000);
+
+        const url = "maliterie.com";
+    
+        jsReader.init(url)
+            .then( r => {
+                
+                let script = _.find( r, ['be2bill', true ] );
+
+                assert.equal(script.be2bill, true);
+
+            })
+            .then(done, done)
+    });
+
+
+
+    it('should find hipay in fr.wikomobile.com ', function(done) {
+    
+        this.timeout(105000);
+
+        const url = "fr.wikomobile.com ";
+    
+        jsReader.init(url)
+            .then( r => {
+                
+                let script = _.find( r, ['hipay', true ] );
+
+                assert.equal(script.hipay, true);
+
+            })
+            .then(done, done)
+    });
+
+
+
+    it('should find hipay in alltricks.fr ', function(done) {
+    
+        this.timeout(105000);
+
+        const url = "www.alltricks.fr";
+    
+        jsReader.init(url)
+            .then( r => {
+                
+                let script = _.find( r, ['hipay', true ] );
+
+                assert.equal(script.hipay, true);
+
+            })
+            .then(done, done)
+    });
+
+
+
+
+    it.skip('should find Atos in m.lahalle.com ', function(done) {
+    
+        this.timeout(105000);
+
+        const url = "m.lahalle.com";
+    
+        jsReader.init(url)
+            .then( r => {
+                
+                let script = _.find( r, ['atos', true ] );
+
+                assert.equal(script.atos, true);
+
+            })
+            .then(done, done)
+    });
+
+
+
+
+
 });
+
+
+
+
+
+describe('Reading Files' , function() {
+
+    it('should find 6 CSS files ', function() {
+
+        let body = fs.readFileSync(process.cwd()+'/tests/assets/index.html');
+
+        const r = jsReader.test.getCSSfromDocument(body,'monbraceletnato.fr');
+
+        assert.equal(r.length, 6);
+            
+    });
+
+
+});
+    
+
